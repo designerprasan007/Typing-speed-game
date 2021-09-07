08726784c8dfb625bbbe0263efe067b0ead69349
Codebug <prasanna2904>
2021-09-07 20:37:59 +0530

    responsive fixed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,7 +4,7 @@ const Login = ({loggedin, Username, UsernameFun}) =>{
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <div className="col-md-6 offset-md-3 col-sm-6 offset-sm-3">
+                    <div className="col-md-6 offset-md-3 col-12 offset-2">
                         <div className="card pt-5">
                             <div className="card-body">
                                 <h4>Lets Start</h4>
